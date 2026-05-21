14edbfdaecf77b382d5a2724e11a0b857d421907
sivaram000 <katta.sivaram@suprsend.com>
2026-05-21 18:36:02 +0530

    added bundle id tracking in add ios push

--- Sources/SuprSend/User.swift
+++ Sources/SuprSend/User.swift
@@ -223,6 +223,7 @@ extension User {
                 .iOSPush: token,
                 .deviceID: getDeviceID(),
                 .pushVendor: Constants.pushVendor,
+                .bundleID: Bundle.main.bundleIdentifier ?? "",
             ]
         )
         config.deviceToken = token
