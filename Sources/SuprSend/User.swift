b464b4be9dea9a55e356fde003b598bb91eacd07
sivaram000 <katta.sivaram@suprsend.com>
2026-05-22 12:01:14 +0530

    user agent added, preferences changes

--- Sources/SuprSend/User.swift
+++ Sources/SuprSend/User.swift
@@ -241,6 +241,7 @@ extension User {
                 .iOSPush: token,
                 .deviceID: getDeviceID(),
                 .pushVendor: Constants.pushVendor,
+                .bundleID: Bundle.main.bundleIdentifier ?? "",
             ]
         )
         return await triggerUserEvent(event)
